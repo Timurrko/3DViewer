bebfc8ac5a3d08635997293c35b7b6fcb51bee55
Timurrko <sharapov.team@yandex.ru>
2020-12-31 15:35:01 +0500

    Added line-drawing

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ running = True
 vectors = [[1, 0, 0], [0, 1, 0], [0, 0, -1]]
 pressed = False
 scale = 64
+lines = []
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((500, 400))
@@ -21,6 +22,28 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 running = False
         what_we_see = list(pygame.key.get_pressed())
+        #print(what_we_see.index(True) if True in what_we_see else 'NF')
+        if what_we_see[20]:
+            line = []
+            for i in range(2):
+                try:
+                    point = list(map(float, input().split()))
+                    if len(point) != 3:
+                        raise TypeError
+                except TypeError:
+                    point = [0, 0, 0]
+                line += point
+            lines.append(line)
+        for line in lines:
+            s_x, s_y, s_z, e_x, e_y, e_z = line
+            s_x, s_y, s_z = vectors[0][0] * s_x + vectors[1][0] * s_y + vectors[2][0] * s_z, \
+                            vectors[0][1] * s_x + vectors[1][1] * s_y + vectors[2][1] * s_z, \
+                            vectors[0][2] * s_x + vectors[1][2] * s_y + vectors[2][2] * s_z
+            e_x, e_y, e_z = vectors[0][0] * e_x + vectors[1][0] * e_y + vectors[2][0] * e_z, \
+                            vectors[0][1] * e_x + vectors[1][1] * e_y + vectors[2][1] * e_z, \
+                            vectors[0][2] * e_x + vectors[1][2] * e_y + vectors[2][2] * e_z
+            pygame.draw.line(screen, (255, 255, 255), (250 + s_x * scale, 200 + s_z * scale),
+                             (250 + e_x * scale, 200 + e_z * scale), 1)
         if what_we_see[26]:
             a1 += pi / 256
         if what_we_see[22]:
