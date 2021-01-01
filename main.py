82a4d6fa172720b0e576e39ce4c3e24b2cab99d7
Timurrko <sharapov.team@yandex.ru>
2021-01-01 22:32:56 +0500

    Added perspective

--- main.py
+++ main.py
@@ -40,22 +40,13 @@ if __name__ == '__main__':
             pygame.draw.line(screen, [(255, 0, 0), (0, 255, 0), (0, 0, 255)][i], (250, 200),
                              (250 + vectors[i][0] * scale, 200 + vectors[i][2] * scale), int(max((scale / 32, 1))))
         for line in lines:
-            s_x, s_z = sum(vectors[:, 0] * line[:3]), sum(vectors[:, 2] * line[:3])
-            e_x, e_z = sum(vectors[:, 0] * line[3:]), sum(vectors[:, 2] * line[3:])
-            pygame.draw.line(screen, (255, 255, 255), (250 + s_x * scale, 200 + s_z * scale),
-                             (250 + e_x * scale, 200 + e_z * scale), int(max((scale / 32, 1))))
-        if what_we_see[26]:
-            a1 += pi / 256
-        if what_we_see[22]:
-            a1 += - pi / 256
-        if what_we_see[4]:
-            a2 += pi / 256
-        if what_we_see[7]:
-            a2 += - pi / 256
-        if what_we_see[45]:
-            scale /= 1.0625
-        if what_we_see[46]:
-            scale *= 1.0625
+            s_x, s_y, s_z = sum(vectors[:, 0] * line[:3]), sum(vectors[:, 1] * line[:3]), sum(vectors[:, 2] * line[:3])
+            e_x, e_y, e_z = sum(vectors[:, 0] * line[3:]), sum(vectors[:, 1] * line[3:]), sum(vectors[:, 2] * line[3:])
+            pygame.draw.line(screen, (255, 255, 255), (250 + s_x * scale * (1.0625 ** (s_y * 1.0625)), 200 + s_z * scale * (1.0625 ** (s_y * 1.0625))),
+                             (250 + e_x * scale * (1.0625 ** (e_y * 1.0625)), 200 + e_z * scale * (1.0625 ** (e_y * 1.0625))), int(max((scale / 32, 1))))
+        a1 += pi / 256 * what_we_see[26] - pi / 256 * what_we_see[22]
+        a2 += pi / 256 * what_we_see[4] - pi / 256 * what_we_see[7]
+        scale = scale / (1.0625 ** what_we_see[45]) * (1.0625 ** what_we_see[46])
         for v in vectors:
             v = v[0] * cos(a2) - v[1] * sin(a2), v[1] * cos(a2) + v[0] * sin(a2), v[2]
             v = v[0], v[1] * cos(a1) + v[2] * sin(a1), v[2] * cos(a1) - v[1] * sin(a1)
