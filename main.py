a61351c2bef42378c81e0cdb829c9faef2e38c99
Timurrko <sharapov.team@yandex.ru>
2021-01-13 22:01:01 +0500

    whole project remade with OOP, added moving and changeable perspective

--- main.py
+++ main.py
@@ -1,57 +1,103 @@
 import pygame
-from math import sin, cos, pi
-from numpy import array
+from numpy import sin, cos, array, tan
+from math import pi
 
-fps = 100  # количество кадров в секунду
-clock = pygame.time.Clock()
+
+def get_spin_matrix(angle):
+    return array([[cos(-angle), -sin(-angle)], [sin(-angle), cos(-angle)]])
+
+
+def generate_vector(h_angle=0, v_angle=0, ad_h_angle=0, length=1):
+    x = 0
+    y = 1
+    z = 0
+    x, y = (sum(i) for i in array([x, y]) * get_spin_matrix(ad_h_angle))
+    z, y = (sum(i) for i in array([z, y]) * get_spin_matrix(v_angle))
+    x, y = (sum(i) for i in array([x, y]) * get_spin_matrix(h_angle))
+    return array([x, y, z]) * length
+
+
+class Camera:
+    def __init__(self, x=0, y=0, z=0, angle_of_view=pi/4, v_angle=0, h_angle=0):
+        self.angle_of_view = angle_of_view
+        self.coords = array([float(x), float(y), float(z)])
+        self.v_angle = v_angle
+        self.h_angle = h_angle
+
+    def rotate(self, v_angle=0, h_angle=0):
+        self.h_angle += h_angle
+        self.h_angle = self.h_angle % (2 * pi)
+        self.v_angle += v_angle
+        self.v_angle = self.v_angle % (2 * pi)
+
+    def move_forward(self, step=0.5):
+        self.coords += generate_vector(self.h_angle, self.v_angle) * step
+
+    def move_back(self, step=0.5):
+        self.coords -= generate_vector(self.h_angle, self.v_angle) * step
+
+    def draw(self, screen, space):
+        width, height = screen.get_size()
+        points = space.get_points()
+        points -= self.coords
+        rotate_points = []
+        for x, y, z in points:
+            x, y = (sum(i) for i in array([x, y]) * get_spin_matrix(-self.h_angle))
+            z, y = (sum(i) for i in array([z, y]) * get_spin_matrix(-self.v_angle))
+            rotate_points.append(array([x, y, z]))
+        for point in rotate_points:
+            if sum(point * array([0, 1, 0])) / (sum(point ** 2) ** 0.5) >= cos(self.angle_of_view):
+                x, y, z = point
+                pygame.draw.circle(screen, (255, 255, 255), (int(width / 2 * (1 + x / (y * tan(self.angle_of_view)))), int(height / 2 * (1 - z / (y * tan(self.angle_of_view))))), max((1, int(15 / y))))
+
+
+class Space:
+    def __init__(self, points):
+        self.points = [array(coords) for coords in points]
+
+    def add_point(self, x=0, y=0, z=0):
+        self.points.append(array(x, y, z))
+
+    def get_points(self):
+        return self.points.copy()
+
+
 running = True
-vectors = array([1, 0, 0, 0, 1, 0, 0, 0, -1])
-vectors.resize(3, 3)
-pressed = False
-scale = 64
-lines = []
+fps = 25
+clock = pygame.time.Clock()
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((500, 400))
-    pygame.display.set_caption('3d')
+    print(screen.get_size())
+    pygame.display.set_caption('3DEngine')
+    camera = Camera(pi / 2.5)
+    space = Space([[i * 0.05, 1, 0] for i in range(-90, 91)])
     while running:
         screen.fill((0, 0, 0))
-        vectors1 = []
-        a1 = 0
-        a2 = 0
+
+        camera.draw(screen, space)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        what_we_see = list(pygame.key.get_pressed())
-        #print(what_we_see.index(True) if True in what_we_see else 'NF')
-        if what_we_see[20]:
-            line = []
-            for i in range(2):
-                try:
-                    point = list(map(float, input().split()))
-                    if len(point) != 3:
-                        raise TypeError
-                except TypeError:
-                    point = [0, 0, 0]
-                line += point
-            lines.append(array(line))
-        for i in range(3):
-            pygame.draw.line(screen, [(255, 0, 0), (0, 255, 0), (0, 0, 255)][i], (250, 200),
-                             (250 + vectors[i][0] * scale, 200 + vectors[i][2] * scale), int(max((scale / 32, 1))))
-        for line in lines:
-            s_x, s_y, s_z = sum(vectors[:, 0] * line[:3]), sum(vectors[:, 1] * line[:3]), sum(vectors[:, 2] * line[:3])
-            e_x, e_y, e_z = sum(vectors[:, 0] * line[3:]), sum(vectors[:, 1] * line[3:]), sum(vectors[:, 2] * line[3:])
-            pygame.draw.line(screen, (255, 255, 255), (250 + s_x * scale * (1.0625 ** (s_y * 1.0625)), 200 + s_z * scale * (1.0625 ** (s_y * 1.0625))),
-                             (250 + e_x * scale * (1.0625 ** (e_y * 1.0625)), 200 + e_z * scale * (1.0625 ** (e_y * 1.0625))), int(max((scale / 32, 1))))
-        a1 += pi / 256 * what_we_see[26] - pi / 256 * what_we_see[22]
-        a2 += pi / 256 * what_we_see[4] - pi / 256 * what_we_see[7]
-        scale = scale / (1.0625 ** what_we_see[45]) * (1.0625 ** what_we_see[46])
-        for v in vectors:
-            v = v[0] * cos(a2) - v[1] * sin(a2), v[1] * cos(a2) + v[0] * sin(a2), v[2]
-            v = v[0], v[1] * cos(a1) + v[2] * sin(a1), v[2] * cos(a1) - v[1] * sin(a1)
-            vectors1.append(array(v))
-        vectors = array(vectors1)
+        pressed_keys = list(pygame.key.get_pressed())
+        print(pressed_keys.index(True) if True in pressed_keys else 'NF')
+        if pressed_keys[26]:
+            camera.move_forward(0.125)
+        if pressed_keys[7]:
+            camera.rotate(h_angle=pi/32)
+        if pressed_keys[4]:
+            camera.rotate(h_angle=-pi/32)
+        if pressed_keys[22]:
+            camera.move_back(0.125)
+        if pressed_keys[82]:
+            camera.rotate(v_angle=pi/32)
+        if pressed_keys[81]:
+            camera.rotate(v_angle=-pi/32)
+        if pressed_keys[46]:
+            camera.angle_of_view *= 1.0625
+        if pressed_keys[45]:
+            camera.angle_of_view /= 1.0625
 
         pygame.display.flip()
         clock.tick(fps)
